7f685bfd1064d998b9b08d9cd1c0401aff587ffc
dcoxmob <dcox@mobiquityinc.com>
2018-05-19 17:56:37 -0400

    git@github.com:DCox2016/AngularJScode.git

--- plunk-oWHWxT0Dm8LP5939w5wr/script.js
+++ plunk-oWHWxT0Dm8LP5939w5wr/script.js
@@ -1,28 +1,35 @@
-//
 (function() {
-  
-  var app = angular.module("githubViewer", []);
-  
+
+  var app = angular.module("githubViewer", [ ]);
+
   var MainCtrl = function(n, $http) {
 
-    var onUserComplete = function(response) {
+   var onUserComplete = function(response) {
       n.user = response.data;
+      $http.get(n.user.repos_url)
+        .then(onRepos, onError);
     };
 
+    var onRepos = function(response){
+      
+      n.repos = response.data;
+    }  
+    
     var onError = function(reason) {
       n.error = "Could not find that user.";
     };
 
     n.search = function(username) {
-    $http.get("https://api.github.com/users/" + username)
-      .then(onUserComplete, onError);
-    };  
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    };
 
     n.username = ""
     n.message = "Github Profile Viewer";
+    n.repoSortOrder = "+name";
 
   };
-  
+
   app.controller("MainCtrl", ["$scope", "$http", MainCtrl]);
 
 }());
